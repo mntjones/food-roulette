4fd442b9b485719718edf7408a1390e0b96c73db
Monica <mntamaru@gmail.com>
2018-07-05 22:02:26 -0700

    added users controller logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,14 @@ class ApplicationController < ActionController::Base
 
 	end
 	
+	private
+
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    redirect_to '/' unless current_user
+  end
+  
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,34 @@
 class UsersController < ApplicationController
+	def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.create(user_params)
+		if @user 
+			session[:user_id] = @user.id
+    	redirect_to user_path(@user)
+    else
+    	# add flash message to complete form
+    	redirect_to(controller: 'users', action: 'new')
+    end
+	end
+
+	def show
+		@user = User.find_by(id: params[:id])
+		if @user == current_user
+			redirect_to user_path(@user)
+		else
+			#add flash message to log in
+			redirect_to root_path
+		end
+	end
+
+
+	private 
+
+	def user_params
+		params.require(:user).permit(:email, :password, :admin)
+	end
+
 end

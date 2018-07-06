8fb8e78adc6a9af60f9c05f9340ad27cd8db30eb
Monica <mntamaru@gmail.com>
2018-07-05 22:30:35 -0700

    getting sign up to work

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,8 @@ class SessionsController < ApplicationController
 	end
 
 	def create
-		@user = User.find_by(name: params["user"]["name"])
-    if @user.authenticate(params["user"]["password"])
+		@user = User.find_by(name: params["name"])
+    if @user.authenticate(params["password"])
       session[:user_id] = @user.id
       redirect_to(controller: 'users', action: 'show')
     else 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
 	def show
 		@user = User.find_by(id: params[:id])
 		if @user == current_user
-			redirect_to user_path(@user)
+			@message = "At Home - no restaurants yet."
 		else
 			#add flash message to log in
 			redirect_to root_path

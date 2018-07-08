35f16b4a757be3f6389f94dca8709df19a4d204e
Monica <mntamaru@gmail.com>
2018-07-07 17:18:43 -0700

    started restaurants controller

--- app/controllers/resturants_controller.rb
+++ app/controllers/resturants_controller.rb
@@ -1,2 +1,38 @@
 class ResturantsController < ApplicationController
+
+	def new
+		@restaurant = Restaurant.new
+		@user = current_user
+	end
+
+	def create
+		@restaurant = Restaurant.create(restaurant_params)
+		redirect_to restaurant_path
+	end
+
+	def show
+		@restaurant = Restaurant.find_by(id: params[:id])
+
+	end
+
+	def edit
+		@restaurant = Restaurant.find_by(id: params[:id])
+	end
+
+	def update
+		@restaurant = Restaurant.find_by(id: params[:id])
+		@restaurant.update(restaurant_params)
+		redirect_to restaurant_path
+	end
+
+	def destroy
+
+	end
+
+	private 
+
+	def restaurant_params
+		params.require(:restaurant).permit(:name, :food_type, :meal_type)
+	end
+
 end
